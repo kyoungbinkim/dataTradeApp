a53b0fd72f9f1a485d533e44e367c2bd7b7df36a
kim <kyoungbin1996@gmail.com>
2023-02-24 18:03:12 +0900

    안드로이드 스마트컨트랙트 연결 수정

--- src/core/http/http.js
+++ src/core/http/http.js
@@ -7,7 +7,7 @@ const httpCli = axios.create();
 const androidURL  = 'http://10.0.2.2:10801';
 const iosURL      = 'http://127.0.0.1:10801';
 
-httpCli.defaults.baseURL = Platform.OS === 'ios' ? iosURL : androidURL;;
+httpCli.defaults.baseURL = Platform.OS === 'ios' ? iosURL : androidURL;
 // httpCli.defaults.withCredentials = true;
 httpCli.defaults.timeout = 250000;
 
--- src/core/web3/index.js
+++ src/core/web3/index.js
@@ -1,12 +1,19 @@
 import _ from 'lodash';
 import contract from "./contract.js";
 import httpCli from '../http/http.js';
+import { Platform } from 'react-native';
 
 let tradeContract = undefined
 export let isInit = false
 
+const androidURL  = 'http://10.0.2.2:7545';
+const iosURL      = 'http://127.0.0.1:7545';
+
+const rpcURL = Platform.OS === 'ios' ? iosURL : androidURL;
+
 export const initTradeContract = async () => {
     let contractAddr = undefined
+    let tmp = undefined;
     // if(isInit) return;
     try {
         if(isInit) return;
@@ -20,9 +27,18 @@ export const initTradeContract = async () => {
         isInit = true
     } catch (error) {
         console.log(error)
+        return;
     }
-    tradeContract = new contract('http://127.0.0.1:7545', contractAddr)
-    console.log(await tradeContract.isRegisteredData('123'))
+
+    tradeContract = new contract(rpcURL, contractAddr);
+
+    try {
+        console.log(await tradeContract.isRegisteredData('123'))
+    } catch (error) {
+        isInit = false;
+        return;
+    }
+    
 }
 
 export const getTradeContract = () => {
