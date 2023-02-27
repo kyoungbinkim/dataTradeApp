c4a459eaf476177e89acd4d1a9d9fc2f4a9f729c
kim <kyoungbin1996@gmail.com>
2023-02-27 13:24:10 +0900

    web3 코드 정리🧹

--- src/core/web3/index.js
+++ src/core/web3/index.js
@@ -3,7 +3,7 @@ import contract from "./contract.js";
 import httpCli from '../http/http.js';
 import { Platform } from 'react-native';
 
-let tradeContract = undefined
+export let tradeContract = undefined
 export let isInit = false
 
 const androidURL  = 'http://10.0.2.2:7545';
@@ -13,14 +13,12 @@ const rpcURL = Platform.OS === 'ios' ? iosURL : androidURL;
 
 export const initTradeContract = async () => {
     let contractAddr = undefined
-    let tmp = undefined;
-    // if(isInit) return;
+    if(isInit) return;
     try {
         if(isInit) return;
         console.log('start init TradeContract')
         const contractInfo = await httpCli.get('/contract/getAddr')
 
-        // console.log("!! : ",contractInfo);
         console.log(contractInfo['data'])
         contractAddr = contractInfo['data']
         
@@ -29,9 +27,9 @@ export const initTradeContract = async () => {
         console.log(error)
         return;
     }
-
     tradeContract = new contract(rpcURL, contractAddr);
 
+    // test contract instance
     try {
         console.log(await tradeContract.isRegisteredData('123'))
     } catch (error) {
